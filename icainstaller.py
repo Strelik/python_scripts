4fc6b33e46ab4a6b1de9a3cd5537fd3d53327c41
Strelik <jason@jrdoppes.com>
2015-12-17 21:04:49 -0500

    updated for dnf and newest ica version

--- icainstaller.py
+++ icainstaller.py
@@ -10,41 +10,36 @@ password = getpass.getpass(prompt="Input your Sudo Password: " )
 
 #Shell CMDS Variables
 tmpdir = '[ ! -d /opt/ica ] && sudo mkdir -p /opt/ica'
-space = "df -k"
 kernel = "uname -r | grep -oP '(\d)\.\d+'"
 distrocmd = "cat /etc/*-release | grep -oP '(^NAME=(Fedora))' | grep -oP '(Fedora)$'"
-fusioncmd = 'sudo yum localinstall --nogpgcheck http://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm http://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm'
-rpminstallcmd = "sudo wget -O /opt/ica/linuxxica-13.1.tar.gz https://googledrive.com/host/0B_RKYajewrzSd0ZxNVFvY1pZNVU && cd /opt/ica && sudo tar -xvzf /opt/ica/linuxxica-13.1.tar.gz && sudo /opt/ica/setupwfc"
+fusioncmd = 'sudo dnf install http://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm http://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm'
+rpminstallcmd = "sudo wget -O /opt/ica/linuxxica-13.3.tar.gz https://googledrive.com/host/0B_RKYajewrzSNVltd0NQcThxbkU && cd /opt/ica && sudo tar -xvzf /opt/ica/linuxxica-13.3.tar.gz && sudo /opt/ica/setupwfc"
 copypemexportcmd = "cd /opt/Citrix/ICAClient/keystore/cacerts && sudo wget https://certs.godaddy.com/repository/gd-class2-root.crt "
 
 #Global Functions
 
-#ICA Client requires a kernel newer then 2.6.29 to run which is why this is checked for.
 def GetKernel():
 	kernel_value = float(subprocess.check_output(kernel, shell=True))
 	if kernel_value < 2.6 :
-		print("Your kernel is not new enough. Requires Kernel 2.6.29 or higher to run the ICA client\n")
+		print("Your kernel is not new enough. Requires Kernel 2.6.29 or higher.")
 		sys.exit()
 	else:
-		print ("Your kernel is new enough. Go you. Moving on\n")
+		print ("Your kernel is new enough. Proceeding with install.\n")
 
-
-# Get your distribution name and ensure it's Fedora.
 def GetDistro():
 
 	distro_value = str(subprocess.check_output(distrocmd, shell=True))
 
 	return (distro_value)
 
-#Runs a long yum local install to grab the latest Fusion repository for Fedora
 def InstallFusion():
 	subprocess.check_output(fusioncmd, shell=True)
 
 
-#Using an "or" operator causes any other input to be allowed past the loop. Not sure why. 
+# Using an "or" operator causes any other input to be allowed past the loop.
 def InstallPrompt1():
 
-	ask_yum = input("Use Yum to install all the necessary dependencies? Type Y or N: ")
+	ask_yum = input("\nUse Yum to install all the necessary dependencies? Type Y or N: ")
 
 	while True:
 		if ask_yum == "Y":
@@ -52,7 +47,7 @@ def InstallPrompt1():
 		elif ask_yum == "N":
 			break
 		else:
-			ask_yum = input("Use Yum to install all the necessary dependencies? Type Y or N: ")
+			ask_yum = input("\nUse Yum to install all the necessary dependencies? Type Y or N: ")
 
 	return ask_yum
 
@@ -81,9 +76,9 @@ def main():
     distro_value = GetDistro()
 
     if "Fedora" in distro_value:
-        print("Proceeding with a Fedora installation of ICA Client")
+        print("Proceeding with a Fedora installation of ICA Client.\n")
     else:
-        print("Distro Not Recognized.")
+        print("\nDistro Is Not Fedora.\n")
         sys.exit()
 
     print("\nChecking if Fusion Repository is added, if not let's add it\n")
@@ -102,10 +97,10 @@ def main():
 
     ICAInstall()
 
-#Install the GoDaddy security certificate from the web so the ICA client can trust the connection..
+# Install the GoDaddy security certificate from the web so ICA trusts it.
     InstallCert()
 
-    print("Cool, Citrix Receiver should be installed and working on your system.\n")
+    print("\nCitrix Receiver should be installed and working on your system.\n")
 
 if __name__ == '__main__':
     sys.exit(main())
